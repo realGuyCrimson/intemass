a9af4082138812947564c81037d10f174b40e4fa
Guy Crimson <theredprimordial@gmail.com>
2025-11-09 18:11:13 +0000

    generate more profiles

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -28,5 +28,19 @@ export const USERS: User[] = [
         email: 'olivia.s@university.com',
         role: 'student',
         avatarUrl: 'https://images.unsplash.com/photo-1616002411355-49593fd89721?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHx3b21hbiUyMGZhY2V8ZW58MHx8fHwxNzYyNjA5NDk5fDA&ixlib=rb-4.1.0&q=80&w=1080'
+    },
+    {
+        id: 'user-4',
+        name: 'Noah Williams',
+        email: 'noah.w@university.com',
+        role: 'student',
+        avatarUrl: 'https://images.unsplash.com/photo-1560250097-0b93528c311a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxtYW4lMjBwb3J0cmFpdHxlbnwwfHx8fDE3NjI2NDU4MTJ8MA&ixlib=rb-4.1.0&q=80&w=1080'
+    },
+    {
+        id: 'user-5',
+        name: 'Emma Brown',
+        email: 'emma.b@university.com',
+        role: 'student',
+        avatarUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxwZXJzb24lMjBwb3J0cmFpdHxlbnwwfHx8fDE3NjI1OTQ3NDd8MA&ixlib=rb-4.1.0&q=80&w=1080'
     }
 ];
